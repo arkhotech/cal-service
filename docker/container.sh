b59e19163fe1763dfb0b674d057b758cc4b784fc
Marcelo Silva <msilva@arkhotech.com>
2016-08-29 14:15:29 -0300

    Script para el control de containers

---
+++ docker/container.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+
+
+CONTAINER=$2
+
+if [ -z $2 ];
+then
+	echo "No se ha especificado el valor del container"
+	exit 1
+fi
+
+ VAL=$(docker inspect $CONTAINER 2  > /dev/null) 
+
+if [ $? -eq 1 ];
+then
+	echo "Ccontenedor $CONTAINER no existe"
+	exit 0
+fi
+
+
+case $1 in
+
+'start')
+	echo "start"
+	docker start $CONTAINER
+;;
+
+
+'stop')
+	echo "stop"	
+	docker stop $CONTAINER
+;;
+'restart')
+	echo "restart"
+	docker restart $CONTAINER
+;;
+*)
+	echo "Comando desconocido"
+
+esac
